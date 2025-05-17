93dc6e6933508fd2c3f8c07d56075c3bc6d87def
alperenbcv <alperenbcv@gmail.com>
2025-05-18 01:33:46 +0300

    bugs polished.

--- compas_ui.py
+++ compas_ui.py
@@ -23,35 +23,35 @@ with tab2:
     - **TCK 81**: Intentional Killing
     - **TCK 81**: Qualified cases  
     """)
-    st.info("""TCK Article 25: Legitimate Defence and Necessity ->
+    st.info("""TCK Article 25: Legitimate Defence and Necessity ->\n
      (1)-No penalty shall be imposed upon an offender in respect of acts which were necessary to repel an unjust assault which is directed, carried out, certain to be carried out or to be repeated against a right to which he, or another, was entitled, provided such acts were proportionate to the assault, taking into account the situation and circumstances prevailing at the time. 
      (2)-No penalty shall be imposed upon an offender in respect of acts which were committed out of necessity, in order to protect against a serious and certain danger (which he has not knowingly caused) which was directed at a right to which he, or another, was entitled and where there was no other means of protection, provided that the means used were proportionate to the gravity and subject of the danger. """)
 
-    st.info("""TCK Article 28: Force and Violence, Menace and Threat ->
+    st.info("""TCK Article 28: Force and Violence, Menace and Threat ->\n
      (1)-No penalty shall be imposed upon a person who commits a criminal offence as a result of intolerable or inevitable violence, or serious menace or gross threat. In such cases, the person involved in the use of force, violence, menace or threat shall be deemed to be the offender. """)
-    st.info("""TCK Article 29: Unjust Provocation ->
+    st.info("""TCK Article 29: Unjust Provocation ->\n
     (1)-Any person who commits an offence in a state of anger or severe distress caused by an unjust act shall be sentenced to a penalty of imprisonment for a term of eighteen to twenty four years where the offence committed requires a penalty of aggravated life imprisonment and to a penalty of imprisonment for a term of twelve to eighteen years where the offence committed requires a penalty of life imprisonment. Otherwise the penalty to be imposed shall be reduced by one-quarter to three- quarters. 
     """)
-    st.info("""TCK Article 30: Mistake ->
+    st.info("""TCK Article 30: Mistake ->\n
     (1)-Any person who, while conducting an act, is unaware of matters which constitute the actus reus of an offence, is not considered to have acted intentionally. Culpability with respect to recklessness shall be preserved in relation to such mistake. 
     (2)-Any person who is mistaken about matters which constitute an element of a qualified version of an offence, which requires an aggravated or mitigated sentence, shall benefit from such mistake.
     (3)-Any person who is inevitably mistaken about the conditions which, when satisfied, reduce or negate culpability shall benefit from such mistake.""")
-    st.info("""TCK Article 31: Minors ->
+    st.info("""TCK Article 31: Minors ->\n
     (1)-Minors under the age of twelve are exempt from criminal liability. While such minors cannot be prosecuted, security measures in respect of minors may be imposed. 
     (2)-(Amended on 29 June 2005 – By Article 5 of the Law no. 5377). Where a minor is older than twelve, but younger than fifteen, at the time of an offence, and he is either incapable of appreciating the legal meaning and consequences of his act or his capability to control his behavior is underdeveloped then he is shall be exempt from criminal liability. However, such minors may be subject to security measures specific to children. Where the minor has the capability to comprehend the legal meaning and result of the act and to control his behaviors in respective of his act, for offences requiring a penalty of aggravated life imprisonment, a term of twelve to fifteen years of imprisonment shall be imposed and for offences that require a penalty of life imprisonment, a term of nine to eleven years imprisonment shall be imposed. Otherwise the penalty to be imposed shall be reduced by half, save for the fact that for each act such penalty shall not exceed seven years.
     (3)-(Amended on 29 June 2005 – By Article 5 of the Law no. 5377). Where a minor is older than fifteen but younger than eighteen years at the time of the offence then for crimes that require a penalty of aggravated life imprisonment a term of eighteen to twenty four years of imprisonment shall be imposed and for offences that require a penalty of life imprisonment twelve to fifteen years of imprisonment shall be imposed. Otherwise the penalty to be imposed shall be reduced by one-third, save for the fact that the penalty for each act shall not exceed twelve years.
     """)
-    st.info("""TCK Article 32: Mental Disorder ->
+    st.info("""TCK Article 32: Mental Disorder ->\n
     (1)-A penalty shall not be imposed on a person who, due to mental disorder, cannot comprehend the legal meaning and consequences of the act he has committed, or if, in respect of such act, his ability to control his own behaviour was significantly diminished. However, security measures shall be imposed for such persons.
     (2)-Notwithstanding that it does not reach the extent defined in paragraph one, where a person’s ability to control his behaviour in respect of an act he has committed is diminished then a term of imprisonment for a term of twenty-five years where the offence committed requires a penalty of aggravated life imprisonment shall be imposed. Otherwise the penalty to be imposed may be reduced by no more than one-sixth. The penalty to be imposed may be enforced partially or completely as a security measure specific to mentally disordered persons, provided the length of the penalty remains the same.
     """)
-    st.info("""TCK Article 33: Deafness and Muteness ->
+    st.info("""TCK Article 33: Deafness and Muteness ->\n
     (1)-The provisions of this law which relate to minors under twelve years of age at the date of the offence shall also be applicable to deaf and mute persons under the age of fifteen. The provisions of this law which relate to minors who are over twelve years of age but under fifteen shall also be applicable to deaf and mute persons who are over fifteen years of age but under eighteen years of age. The provisions of this law which relate to minors over fifteen years of age but under eighteen of age shall be applied to deaf and mute persons who are over eighteen years of age but under twenty years of age.
     """)
-    st.info("""TCK Article 34: Transitory Reasons and Being under Influence of Alcohol or Drugs
+    st.info("""TCK Article 34: Transitory Reasons and Being under Influence of Alcohol or Drugs ->\n
     (1)-Any person who is, because of a transitory reason or the effect of alcohol or drugs taken involuntarily, unable to comprehend the legal meaning and consequences of an act he has committed, or whose ability to control his behaviour regarding such act was significantly diminished, shall not be subject to a penalty.
     (2)-The provisions of the paragraph one shall not apply to a person who commits an offence under the effects of alcohol or drugs which have been taken voluntarily.""")
-    st.info("""TCK Article 61: Determination of the Penalty ->
+    st.info("""TCK Article 61: Determination of the Penalty ->\n
     (1)-In a particular case, the judge shall determine the basic penalty, between the minimum and maximum limits of the offence as defined by law, by considering the following factors:
         (a)-the manner in which the offence was committed;
         (b)-the means used to commit it;
@@ -64,9 +64,9 @@ with tab2:
     (5)-The penalty according to the above paragraphs will be finally determined by taking the following into consideration and in this order: attempt; jointly-committed offences; successive offences; unjust provocation; minor status; mental disorder, personal circumstances requiring a reduction of the penalty and discretionary mitigation.
     (7)-(Added on 29 June 2005 – By Article 7 of the Law no. 5377) The final penalty determined under this article for an offence that requires a specific of imprisonment, shall not exceed thirty years.
     (10)-Unless explicitly written in the law, penalties cannot be increased, decreased or converted.""")
-    st.info("""TCK Article 81: Intentional Killing ->
+    st.info("""TCK Article 81: Intentional Killing ->\n
     (1)-Any person who intentionally kills another shall be sentenced to life imprisonment.""")
-    st.info("""TCK Article 82: Qualified cases ->
+    st.info("""TCK Article 82: Qualified cases ->\n
     (1)-If the act of intentional killing is committed: 
         (a)-With premeditation,
         (b)-Brutally or through torment;
