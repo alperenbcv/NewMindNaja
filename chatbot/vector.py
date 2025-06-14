26946dc199638c79fb775e09837358a861c1dd54
alperenbcv <alperenbcv@gmail.com>
2025-06-14 20:14:50 +0300

    bug fix

--- chatbot/vector.py
+++ chatbot/vector.py
@@ -1,8 +1,8 @@
-# chatbot/vector.py  (güncel)
 from langchain_neo4j import Neo4jVector
 from chatbot.llm   import embedding_model
 from chatbot.graph import graph
 
+# ── VektörStore nesnesi ──────────────────────────────────────────
 vs = Neo4jVector(
     embedding=embedding_model,
     graph=graph,
@@ -12,6 +12,12 @@ vs = Neo4jVector(
     embedding_node_property="embedding",
 )
 
+
+retriever = vs.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3},   )
+
 def get_similar_karar_by_embedding(query: str, k: int = 3) -> str:
+
     docs = vs.similarity_search(query, k=k)
     return "\n\n".join(d.page_content for d in docs)
